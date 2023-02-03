77023109b93db526ca0f27863e30d6cdea77701b
Alex <cutting.net@yandex.ru>
2023-02-03 13:22:51 +0700

    upgrade AddTeamFragment(write some functions) create gameViewModel

--- app/src/main/java/com/alex_cutnet/aliasgame/scenes/AddTeamViewModel.kt
+++ app/src/main/java/com/alex_cutnet/aliasgame/scenes/AddTeamViewModel.kt
@@ -1,17 +1,37 @@
 package com.alex_cutnet.aliasgame.scenes
 
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 
 class AddTeamViewModel : ViewModel() {
+
+    private var _nicknamesForTeams = MutableLiveData<List<String>>()
+    val nicknamesForTeams: LiveData<List<String>>
+        get() = _nicknamesForTeams
+
+    private val nicknamesFinalList = mutableListOf<String>()
 
     fun checkNicknamesNotDuplicates(nicknamesList: List<String>): List<String> {
         val localNicknamesList = mutableListOf<String>()
 
         for (nickname in nicknamesList) {
-            if (!localNicknamesList.contains(nickname))
-                localNicknamesList.add(nickname)
-            else continue
+            if (localNicknamesList.size != MAX_SUM_TEAMS) {
+                if (!localNicknamesList.contains(nickname)) {
+                    localNicknamesList.add(nickname)
+                } else continue
+            } else break // break if list already full
         }
+
+        nicknamesFinalList.intersect(localNicknamesList.toSet())
         return localNicknamesList
+    }
+
+    fun getNicknamesInLiveData() {
+        _nicknamesForTeams.value = nicknamesFinalList
+    }
+
+    companion object {
+        private const val MAX_SUM_TEAMS = 5
     }
 }
